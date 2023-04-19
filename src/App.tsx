fc6a61171fa4c724439bbf5d6a3331067c7b1ce2
diegonacif <diegoreisnacif@gmail.com>
2023-04-18 21:58:13 -0300

    feat: menu component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import { Header } from './components/Header/Header'
+import { Menu } from './components/Menu/Menu'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   )
 }
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+
+export const Menu = () => {
+  return (
+    <div className="menu-container">
+      <h4 className="menu-item selected">Atributos</h4>
+      <h4 className="menu-item ">Vantagens</h4>
+      <h4 className="menu-item ">Desvantagens</h4>
+      <h4 className="menu-item ">Perícias</h4>
+      <h4 className="menu-item ">Equipamentos</h4>
+    </div>
+  )
+}
