1f26015310e10847ae731fbf1d938fd8b758f8fc
diegonacif <diegoreisnacif@gmail.com>
2023-04-19 20:26:36 -0300

    feat: attributes hexagon inputs

--- src/components/Attributes/Attributes.tsx
+++ src/components/Attributes/Attributes.tsx
@@ -1,51 +1,69 @@
 import '../../App.scss';
 
 export const Attributes = () => {
+
   return (
     <div className="attributes-container">
       <h3 id="attributes-title">Atributos</h3>
+      
       <div className="attributes-wrapper">
         <div className="attributes-primary">
           <div className="attribute-wrapper">
             <span>For</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Des</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Int</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Vit</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
         </div>
         <div className="attributes-secondary">
           <div className="attribute-wrapper">
             <span>PV</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Vont</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Per</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
           <div className="attribute-wrapper">
             <span>PF</span>
-            <input type="number" />
+            <div className="hexagon">
+              <input type="number" /> 
+            </div>
             <span>0</span>
           </div>
         </div>
