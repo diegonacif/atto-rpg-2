6d8337a07f35a9eaec04bf0c5deccb128d681d47
diegonacif <diegoreisnacif@gmail.com>
2023-04-20 19:59:56 -0300

    feat: skills items created

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,7 +1,85 @@
-import React from 'react'
+import '../../App.scss';
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className="skills-container">
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+      <div className="skills-row">
+        <input type="text" id="skill-1-name" />
+        <input type="text" id="skill-1-mods" />
+        <input type="text" id="skill-1-nh" />
+        <input type="text" id="skill-1-relative" />
+        <input type="text" id="skill-1-points" />
+      </div>
+    </div>
   )
 }
