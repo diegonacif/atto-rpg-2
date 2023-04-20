5c309de75b29d2f0b94846cfc030d521b58482dc
diegonacif <diegoreisnacif@gmail.com>
2023-04-19 21:29:30 -0300

    feat: menu with seleted route

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,12 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Menu = () => {
+  const [currentMenu, setCurrentMenu] = useState("attributes");
+  const navigate = useNavigate();
+
+  const handleCurrentMenu = (currentMenu: string) => {
+    setCurrentMenu(currentMenu)
+    navigate(currentMenu === "attributes" ? "/" : currentMenu)
+  }
+
+  console.log(currentMenu);
+  
   return (
     <div className="menu-container">
-      <h4 className="menu-item selected">Atributos</h4>
-      <h4 className="menu-item ">Vantagens</h4>
-      <h4 className="menu-item ">Desvantagens</h4>
-      <h4 className="menu-item ">Perícias</h4>
-      <h4 className="menu-item ">Equipamentos</h4>
+      <h4 
+        className={`menu-item ${currentMenu === "attributes" && "selected"}`}
+        onClick={() => handleCurrentMenu("attributes")}
+      >
+        Atributos
+      </h4>
+      <h4 
+        className={`menu-item ${currentMenu === "perks" && "selected"}`}
+        onClick={() => handleCurrentMenu("perks")}
+      >
+        Vantagens
+      </h4>
+      <h4 
+        className={`menu-item ${currentMenu === "flaws" && "selected"}`}
+        onClick={() => handleCurrentMenu("flaws")}
+      >
+        Desvantagens
+      </h4>
+      <h4 
+        className={`menu-item ${currentMenu === "skills" && "selected"}`}
+        onClick={() => handleCurrentMenu("skills")}
+      >
+        Perícias
+      </h4>
+      <h4 
+        className={`menu-item ${currentMenu === "equips" && "selected"}`}
+        onClick={() => handleCurrentMenu("equips")}
+      >
+        Equipamentos
+      </h4>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<App />}>
-          <Route path={"attributes"} element={<Attributes />}/>
+          <Route index element={<Attributes />}/>
           <Route path={"perks"} element={<Perks />}/>
         </Route>
       </Routes>
