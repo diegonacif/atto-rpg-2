83df70be73c5c28e166a61f92b4deb52402d90dc
diegonacif <diegoreisnacif@gmail.com>
2023-04-21 11:10:33 -0300

    feat: equips component items done

--- src/components/Attributes/Attributes.tsx
+++ src/components/Attributes/Attributes.tsx
@@ -13,28 +13,28 @@ export const Attributes = () => {
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Des</span>
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Int</span>
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Vit</span>
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
         </div>
         <div className="attributes-secondary">
@@ -43,28 +43,28 @@ export const Attributes = () => {
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Vont</span>
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
           <div className="attribute-wrapper">
             <span>Per</span>
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
           <div className="attribute-wrapper">
             <span>PF</span>
             <div className="hexagon">
               <input type="number" /> 
             </div>
-            <span>0</span>
+            <span className="attribute-cost">0</span>
           </div>
         </div>
       </div>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -11,74 +11,74 @@ export const Skills = () => {
         <input type="text" id="skill-1-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-2-name" />
+        <input type="text" id="skill-2-mods" />
+        <input type="text" id="skill-2-nh" />
+        <input type="text" id="skill-2-relative" />
+        <input type="text" id="skill-2-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-3-name" />
+        <input type="text" id="skill-3-mods" />
+        <input type="text" id="skill-3-nh" />
+        <input type="text" id="skill-3-relative" />
+        <input type="text" id="skill-3-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-4-name" />
+        <input type="text" id="skill-4-mods" />
+        <input type="text" id="skill-4-nh" />
+        <input type="text" id="skill-4-relative" />
+        <input type="text" id="skill-4-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-5-name" />
+        <input type="text" id="skill-5-mods" />
+        <input type="text" id="skill-5-nh" />
+        <input type="text" id="skill-5-relative" />
+        <input type="text" id="skill-5-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-6-name" />
+        <input type="text" id="skill-6-mods" />
+        <input type="text" id="skill-6-nh" />
+        <input type="text" id="skill-6-relative" />
+        <input type="text" id="skill-6-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-7-name" />
+        <input type="text" id="skill-7-mods" />
+        <input type="text" id="skill-7-nh" />
+        <input type="text" id="skill-7-relative" />
+        <input type="text" id="skill-7-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-8-name" />
+        <input type="text" id="skill-8-mods" />
+        <input type="text" id="skill-8-nh" />
+        <input type="text" id="skill-8-relative" />
+        <input type="text" id="skill-8-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-9-name" />
+        <input type="text" id="skill-9-mods" />
+        <input type="text" id="skill-9-nh" />
+        <input type="text" id="skill-9-relative" />
+        <input type="text" id="skill-9-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-10-name" />
+        <input type="text" id="skill-10-mods" />
+        <input type="text" id="skill-10-nh" />
+        <input type="text" id="skill-10-relative" />
+        <input type="text" id="skill-10-points" />
       </div>
       <div className="skills-row">
-        <input type="text" id="skill-1-name" />
-        <input type="text" id="skill-1-mods" />
-        <input type="text" id="skill-1-nh" />
-        <input type="text" id="skill-1-relative" />
-        <input type="text" id="skill-1-points" />
+        <input type="text" id="skill-11-name" />
+        <input type="text" id="skill-11-mods" />
+        <input type="text" id="skill-11-nh" />
+        <input type="text" id="skill-11-relative" />
+        <input type="text" id="skill-11-points" />
       </div>
     </div>
   )
