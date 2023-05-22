5de8070b5f39aff33537642530cf1b7e688c2a39
diegonacif <diegoreisnacif@gmail.com>
2023-05-22 17:49:58 -0300

    feat: character header collapsible

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,37 +1,56 @@
 import { useState } from 'react'
 import { Menu } from '../Menu/Menu'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { PointsResume } from '../PointsResume/PointsResume';
 import { CharacterResume } from '../CharacterResume/CharacterResume';
-import { TrendUp, UserList, YinYang } from '@phosphor-icons/react';
+import { CaretCircleDown, CaretCircleUp, TrendUp, UserList, YinYang } from '@phosphor-icons/react';
+import Collapsible from 'react-collapsible';
 
 export const Character = () => {
-  const navigate = useNavigate();
   const [currentFlip, setCurrentFlip] = useState('points-resume');
+  const [isHeaderOpen, setIsHeaderOpen] = useState(true);
+
   return (
     <div className="character-container">
-      <header className="character-header">
-        {/* <button 
-          className="back-button"
-          onClick={() => navigate("/home")}
-        >
-          Voltar
-        </button> */}
-        {
-          currentFlip === 'character-resume' ?
-          <CharacterResume /> :
-          currentFlip === 'points-resume' ?
-          <PointsResume /> :
-          currentFlip === 'perks-and-flaws-resume' ?
-          <h1>Perks'n'Flaws</h1> :
-          null
+      <Collapsible 
+        trigger={
+          isHeaderOpen ?
+          <CaretCircleUp 
+            size={28} 
+            weight="fill" 
+            className="open-close-character-header"
+            onClick={() => setIsHeaderOpen(false)}
+          /> :
+          <CaretCircleDown 
+            size={28} 
+            weight="fill" 
+            className="open-close-character-header"
+            onClick={() => setIsHeaderOpen(true)}
+          />
         }
-        <div className="nav-menu">
-          <UserList size={32} weight="duotone" onClick={() => setCurrentFlip('character-resume')}/>
-          <YinYang size={32} weight="duotone" onClick={() => setCurrentFlip('perks-and-flaws-resume')}/>
-          <TrendUp size={32} weight="duotone" onClick={() => setCurrentFlip('points-resume')}/>
-        </div>
-      </header>
+        easing="ease"
+        transitionTime={500}
+      >
+        <header className="character-header">
+          <section className="character-header-content">
+            {
+              currentFlip === 'character-resume' ?
+              <CharacterResume /> :
+              currentFlip === 'points-resume' ?
+              <PointsResume /> :
+              currentFlip === 'perks-and-flaws-resume' ?
+              <h1>Perks'n'Flaws</h1> :
+              null
+            }
+          </section>
+          <div className="nav-menu">
+            <UserList size={32} weight="duotone" onClick={() => setCurrentFlip('character-resume')}/>
+            <YinYang size={32} weight="duotone" onClick={() => setCurrentFlip('perks-and-flaws-resume')}/>
+            <TrendUp size={32} weight="duotone" onClick={() => setCurrentFlip('points-resume')}/>
+          </div>
+        </header>
+      </Collapsible>
+      
       <Menu />
       <Outlet />
     </div>
