2a2d1f1ea4c54ca27af299e9e296485cca1dcf04
diegonacif <diegoreisnacif@gmail.com>
2023-05-23 11:50:46 -0300

    feat: character resume structure

--- src/components/CharacterResume/CharacterResume.tsx
+++ src/components/CharacterResume/CharacterResume.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import charImg from '../../assets/charIcons/faceless.png';
 
 export const CharacterResume = () => {
   return (
     <div className="character-resume-container">
-      <div>
-        <img src="" alt="" />
+      <div className="text-wrapper">
+        <span>Nome: Nome</span>
+        <span>Sexo: Sexo</span>
       </div>
-      <div>
-        <h1>Character Resume</h1>
+      <div className="image-wrapper">
+        <img src={charImg} alt="character image" />
+      </div>
+      <div className="text-wrapper">
+        <span>Idade: Idade</span>
+        <span>Altura: Altura</span>
+        <span>Peso: Peso</span>
       </div>
     </div>
   )
