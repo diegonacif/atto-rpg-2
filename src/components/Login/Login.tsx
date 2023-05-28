d57ace71920fe2efef753cbc78f8e500f29667f8
diegonacif <diegoreisnacif@gmail.com>
2023-05-28 09:39:16 -0300

    style: login component restyle

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from 'react';
 import { AuthGoogleContext } from '../../contexts/AuthGoogleProvider';
 import { useNavigate } from 'react-router-dom';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import greenVideo from '../../assets/green-video.mp4';
+import diceImg from '../../assets/d6-dice-green.png';
 
 export const Login = () => {
   const { 
@@ -22,8 +25,24 @@ export const Login = () => {
   
   return (
     <div className="login-container">
-      <div className="login-wrapper">
-        <button onClick={() => handleGoogleSignIn()}>Login com Google</button>
+      <video id="background-video" loop autoPlay muted>
+        <source src={greenVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="hexagon">
+        <div className="title-wrapper" onClick={() => navigate("/home")}>
+          <h2>Atto RPG</h2>
+          <img src={diceImg} alt="d6 dice" id="d6-dice" />
+        </div>
+      </div>
+      <h3>Faça login com sua conta Google</h3>
+      <div 
+        className="login-wrapper"
+        onClick={() => handleGoogleSignIn()}
+      >
+        <AiFillGoogleCircle id="google-logo" size={'1.5rem'} />
+        <span>Continuar com Google</span>
+        {/* <button onClick={() => handleGoogleSignIn()}>Continuar com Google</button> */}
       </div>
     </div>
   )
