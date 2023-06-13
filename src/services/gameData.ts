3b20d585ba29a9a9c862bd18a997cda326e968f0
diegonacif <diegoreisnacif@gmail.com>
2023-06-13 15:35:17 -0300

    feat: new perks added

--- src/services/gameData.ts
+++ src/services/gameData.ts
@@ -17,11 +17,13 @@ interface ISkills {
 
 export const perksData: IPerks[] = [
   { name: "Aparência", levels: [1, 2, 3] },
+  { name: "Ambidestria", levels: [1] },
   { name: "Aptidão Mágica", levels: [1, 2, 3, 4, 5] },
   { name: "Ataque Adicional", levels: [3] },
   { name: "Boa Forma", levels: [1] },
   { name: "Carisma", levels: [1, 2, 3, 4, 5] },
   { name: "Defesas Ampliadas", levels: [1, 2, 3, 4, 5] },
+  { name: "Dobra", levels: [5] },
   { name: "Equilíbrio Perfeito", levels: [4] },
   { name: "Memória Eidética", levels: [3] },
   { name: "Mestre de Armas", levels: [3] },
