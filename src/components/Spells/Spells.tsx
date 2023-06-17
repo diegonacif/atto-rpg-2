0089cab423ffcfabff94cd13f6e748b565f4e9e6
diegonacif <diegoreisnacif@gmail.com>
2023-06-17 17:11:01 -0300

    feat: [resolved #3] - feat: create spells ui

--- src/components/Spells/Spells.tsx
+++ src/components/Spells/Spells.tsx
@@ -3,6 +3,30 @@ import '../../App.scss';
 
 export const Spells = () => {
   return (
-    <div className="spells-container">Spells</div>
+    <div className="spells-container">
+      <div className="spells-row-title">
+        <span className="spells-row-title-item">Descrição</span>
+        <span className="spells-row-title-item">Mod</span>
+        <span className="spells-row-title-item">NH</span>
+        <span className="spells-row-title-item">Pontos</span>
+      </div>
+
+      <div className="skills-row" key={`skill`}>
+        <span onClick={() => console.log("abrir modal")} id="skill-name">uma descrição</span>
+        <span id="skill-level">0</span>
+        <span id="skill-points">12</span>
+        <span id="skill-points">4</span>
+      </div>
+      <div className="skills-row" key={`skill`}>
+        <span onClick={() => console.log("abrir modal")} id="skill-name">uma descrição</span>
+        <span id="skill-level">0</span>
+        <span id="skill-points">12</span>
+        <span id="skill-points">4</span>
+      </div>
+
+      <button onClick={() => console.log("open modal")} id="new-spell-button">
+        <span className="yellow-span">Nova magia</span>
+      </button>
+    </div>
   )
 }
